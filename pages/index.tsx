11720cd9670b505daab0057b9f7ba25617e7b625
Liia Zubova <vardlitmo@gmail.com>
2023-03-09 16:11:50 +0300

    add: button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components";
 import { Htag } from "@/components/Htag/Htag";
 import React from "react";
 
@@ -5,7 +6,10 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Text</Htag>
-
+      <Button appearance="primary" arrow="right">
+        Кнопка
+      </Button>
+      <Button appearance="ghost">Кнопка</Button>
     </>
   );
 }
