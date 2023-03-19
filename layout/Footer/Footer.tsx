b138851289f409b380d7c29a2fe50a704decf199
Liia Zubova <vardlitmo@gmail.com>
2023-03-19 18:17:05 +0300

    add:

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -3,9 +3,9 @@ import styles from "./Footer.module.css";
 import cn from "classnames";
 import {format} from 'date-fns';
 
-export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className}: FooterProps): JSX.Element => {
   return (
-    <footer className={cn(className, styles.footer)} {...props}>
+    <footer className={cn(className, styles.footer)}>
       <div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
       <a href='#' target='_blank'>Пользовательское соглашение</a>
       <a href='#' target='_blank'>Политика конфиденциальности</a>
