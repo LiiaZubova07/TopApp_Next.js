abe527c3db22f9c56037505fe6ee963d83e1de40
Liia Zubova <vardlitmo@gmail.com>
2023-03-23 19:42:24 +0300

    add: menu folder for context

---
+++ layout/Menu/Menu.tsx
@@ -0,0 +1,12 @@
+import styles from "./Menu.module.css";
+import cn from "classnames";
+import {format} from 'date-fns';
+
+export const Menu = (): JSX.Element => {
+  return (
+    <div>
+      
+    </div>
+  )
+
+};
