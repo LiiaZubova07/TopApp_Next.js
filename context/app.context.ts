745b46e12c867c13f91185aea2dfefb2b0accae0
Liia Zubova <vardlitmo@gmail.com>
2023-03-23 21:06:30 +0300

    push

--- context/app.context.ts
+++ context/app.context.ts
@@ -1,5 +1,5 @@
-import { MenuItem } from "@/interfaces/menu.interface";
-import { TopLevelCategory } from "@/interfaces/page.interface";
+import { MenuItem } from "../interfaces/menu.interface";
+import { TopLevelCategory } from "../interfaces/page.interface";
 import { createContext, ReactNode, useState } from "react";
 
 export interface IAppContext {
@@ -24,7 +24,7 @@ export const AppContextProvider = ({
   };
 
   return (
-    <AppContext.Provider value={{ menu: menuState, firstCategory, setMenu }}>
+    <AppContext.Provider (value={{ menu: menuState, firstCategory, setMenu }})>
       {children}
     </AppContext.Provider>
   );
