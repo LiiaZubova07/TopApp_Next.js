e2f58594f75a0ed490f544ce25d71682805270f2
Liia Zubova <vardlitmo@gmail.com>
2023-03-24 20:01:39 +0300

    hell

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,13 +1,13 @@
 import styles from "./Sidebar.module.css";
 import { SidebarProps } from "./Sidebar.props";
 import cn from "classnames";
-import { Menu } from "../Menu/Menu";
 
 
-export const Sidebar = ({...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
   return (
     <div {...props}>
-<Menu/>
+<Sidebar/>
     </div>
   );
 };
+  
