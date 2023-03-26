9d34247840d83ee89c4847fc36a1137cb27f4511
Liia Zubova <vardlitmo@gmail.com>
2023-03-26 18:36:47 +0300

    add: pages/routing

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,23 +1,12 @@
 import styles from "./Menu.module.css";
 import cn from "classnames";
-import { format } from "date-fns";
 import { useContext } from "react";
 import { AppContext } from "@/context/app.context";
 import { FirstLevelMenuItem, PageItem } from "@/interfaces/menu.interface";
-import CoursesIcon from "./icons/courses.svg";
-import ServicesIcon from "./icons/services.svg";
-import BooksIcon from "./icons/books.svg";
-import ProductsIcon from "./icons/products.svg";
-import { TopLevelCategory } from "@/interfaces/page.interface";
+
 import Link from "next/link";
 import { useRouter } from "next/router";
-
-const firstLevelMenu: FirstLevelMenuItem[] = [
-  { route: "courses", name: "Course", icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-  { route: "services", name: "Services", icon: <ServicesIcon />, id: TopLevelCategory.Services },
-  { route: "books", name: "Books", icon: <BooksIcon />, id: TopLevelCategory.Books },
-  { route: "products", name: "Products", icon: <ProductsIcon />, id: TopLevelCategory.Products },
-];
+import { firstLevelMenu } from "@/helpers/helpers";
 
 export const Menu = (): JSX.Element => {
   const { menu, setMenu, firstCategory } = useContext(AppContext);
