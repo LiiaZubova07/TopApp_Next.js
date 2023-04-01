a50a0c5f70b99275a7ef3a3805df0c0be49b8c5e
Liia Zubova <vardlitmo@gmail.com>
2023-04-01 19:46:33 +0300

    Revert "add: reducer is ready"
    This reverts commit 360e59167373cb76715528adce336cef4e3ba705.

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -3,9 +3,14 @@ import { SortEnum, SortProps } from "./Sort.props";
 import SortIcon from "./sort.svg";
 import cn from "classnames";
 
-export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Element => {
+export const Sort = ({
+  sort,
+  setSort,
+  className,
+}: // ...props
+SortProps): JSX.Element => {
   return (
-    <div className={cn(styles.sort, className)}{...props}>
+    <div className={cn(styles.sort, className)}>
       <span
         onClick={() => setSort(SortEnum.Rating)}
         className={cn({
@@ -22,7 +27,7 @@ export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Ele
         })}
       >
         <SortIcon className={styles.sortIcon} />
-        По цене
+        По&nbsp;цене
       </span>
     </div>
   );
