c0280ea2f112fd52e6d153053500091266bbbd58
Liia Zubova <vardlitmo@gmail.com>
2023-04-08 12:41:46 +0300

    add: forms to console

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -1,22 +1,45 @@
+import { useForm, Controller } from "react-hook-form";
 import { Button } from "../Button/Button";
 import { Input } from "../Input/Input";
 import { Rating } from "../Rating/Rating";
+import { IReviewForm } from "./ReviewForm.interface";
 import styles from "./ReviewForm.module.css";
 import { ReviewFormProps } from "./ReviewForm.props";
 import CloseIcon from "./close.svg";
 import cn from "classnames";
 
-export const ReviewForm = ({ productId, className}: ReviewFormProps): JSX.Element => {
+export const ReviewForm = ({ productId, className }: ReviewFormProps): JSX.Element => {
+  const { register, control, handleSubmit } = useForm<IReviewForm>();
+
+  const onSubmit = (data: IReviewForm) => {
+    console.log(data);
+  };
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className={cn(styles.reviewForm, className)}>
-        <Input placeholder="Имя"></Input>
-        <Input className={styles.title} placeholder="Заголовок отзыва"></Input>
+        {/* пропал placeholder в Имя и Заголовок. и, похоже, из-за этого в консоли undefined*/}
+        <Input {...register("name")} placeholder="Имя"></Input>
+        <Input
+          {...register("title")}
+          className={styles.title}
+          placeholder="Заголовок отзыва"
+        ></Input>
         <div className={styles.rating}>
           <span>Оценка:</span>
-          <Rating rating={0} />
+          <Controller
+            control={control}
+            name="rating"
+            render={({ field }) => (
+              <Rating isEditable rating={field.value} setRating={field.onChange} />
+            )}
+          />
         </div>
-        <textarea className={styles.descriptions} placeholder="Текст отзыва"></textarea>
+        <textarea
+          {...register("description")}
+          className={styles.descriptions}
+          placeholder="Текст отзыва"
+        />
         <div className={styles.submit}>
           <Button appearance="primary">Отправить</Button>
           <span className={styles.info}>
@@ -30,6 +53,6 @@ export const ReviewForm = ({ productId, className}: ReviewFormProps): JSX.Elemen
         <div>Спасибо, Ваш отзыв будет опубликован после проверки.</div>
         <CloseIcon className={styles.close} />
       </div>
-    </>
+    </form>
   );
 };
