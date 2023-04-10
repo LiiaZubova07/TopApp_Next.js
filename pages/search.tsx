f13173cfd82b6982b8f37fc0cbfc3f50c84aa41a
Liia Zubova <vardlitmo@gmail.com>
2023-04-10 13:47:23 +0300

    add: requesting

--- pages/search.tsx
+++ pages/search.tsx
@@ -3,6 +3,7 @@ import { withLayout } from "../layout/Layout";
 import { GetStaticProps } from "next";
 import axios from "axios";
 import { MenuItem } from "../interfaces/menu.interface";
+import { API } from "@/helpers/api";
 
 function Search(): JSX.Element {
   return <>Search</>;
@@ -12,12 +13,9 @@ export default withLayout(Search);
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   const firstCategory = 0;
-  const { data: menu } = await axios.post<MenuItem[]>(
-    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
-    {
-      firstCategory,
-    }
-  );
+  const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find,{
+    firstCategory,
+  });
 
   return {
     props: {
