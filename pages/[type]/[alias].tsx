6dc4678205f60342bf7d75826cca03a8d41cacea
Liia Zubova <vardlitmo@gmail.com>
2023-04-18 14:55:48 +0300

    add: meta

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -9,9 +9,21 @@ import { ProductModel } from "@/interfaces/product.interface";
 import { firstLevelMenu } from "@/helpers/helpers";
 import { TopPageComponent } from "@/page-components";
 import { API } from "@/helpers/api";
+import Head from "next/head";
 
 function TopPage({ firstCategory, page, products }: TopPageProps): JSX.Element {
-  return <TopPageComponent firstCategory={firstCategory} page={page} products={products} />;
+  return (
+    <>
+      <Head>
+        <title>{page.metaTitle}</title>
+        <meta name="description" content={page.metaDescription} />
+        <meta property="og:title" content={page.metaTitle} />
+        <meta property="og:description" content={page.metaDescription} />
+        <meta property="og:type" content="article" />
+      </Head>
+      <TopPageComponent firstCategory={firstCategory} page={page} products={products} />
+    </>
+  );
 }
 
 export default withLayout(TopPage);
@@ -19,12 +31,9 @@ export default withLayout(TopPage);
 export const getStaticPaths: GetStaticPaths = async () => {
   let paths: string[] = [];
   for (const m of firstLevelMenu) {
-    const { data: menu } = await axios.post<MenuItem[]>(
-      API.topPage.find,
-      {
-        firstCategory: m.id,
-      }
-    );
+    const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
+      firstCategory: m.id,
+    });
     paths = paths.concat(menu.flatMap((s) => s.pages.map((p) => `/${m.route}/${p.alias}/`)));
   }
 
@@ -51,12 +60,9 @@ export const getStaticProps: GetStaticProps<TopPageProps> = async ({
   }
 
   try {
-    const { data: menu } = await axios.post<MenuItem[]>(
-      API.topPage.find,
-      {
-        firstCategory: firstCategoryItem.id,
-      }
-    );
+    const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
+      firstCategory: firstCategoryItem.id,
+    });
 
     if (menu.length == 0) {
       return {
@@ -64,17 +70,12 @@ export const getStaticProps: GetStaticProps<TopPageProps> = async ({
       };
     }
 
-    const { data: page } = await axios.get<TopPageModel>(
-      API.topPage.byAlias + params.alias
-    );
+    const { data: page } = await axios.get<TopPageModel>(API.topPage.byAlias + params.alias);
 
-    const { data: products } = await axios.post<ProductModel[]>(
-      API.product.find,
-      {
-        category: page.category,
-        limit: 10,
-      }
-    );
+    const { data: products } = await axios.post<ProductModel[]>(API.product.find, {
+      category: page.category,
+      limit: 10,
+    });
 
     return {
       props: {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import React from "react";
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
+export default function App({ Component, pageProps, router }: AppProps): JSX.Element {
   return (
     <>
       <Head>
         <title>MyTop - the best top</title>
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+        <meta property="og:locale" content="ru_Ru" />
       </Head>
       <Component {...pageProps} />
     </>
